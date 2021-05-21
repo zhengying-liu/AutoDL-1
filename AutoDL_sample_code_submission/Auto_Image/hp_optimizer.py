fa06e910d0e704acce51d6f232a915946e4de5b5
Zhengying Liu <zhengying.liu@polytechnique.edu>
2021-05-21 08:39:09 +0000

    Initial model_final and hp_optimizer

--- AutoDL_sample_code_submission/Auto_Image/hp_optimizer.py
+++ AutoDL_sample_code_submission/Auto_Image/hp_optimizer.py
@@ -1,15 +1,29 @@
-from automl_workflow.api import HPOptimizer
+from automl_workflow.api import HPOptimizer, Learner
 
+from learner import MyLearner
 from architectures.resnet import ResNet9, ResNet18
 
 
 class MyHPOptimizer(HPOptimizer):
 
-    def fit(self, training_info):
+    def fit(self, training_info: dict) -> Learner:
         in_channels = training_info['dataset']['shape'][-1]
         num_class = training_info['dataset']['num_class']
 
         if training_info['loop']['epoch'] == 2 and training_info['need_switch']:
-            return ResNet9(in_channels, num_class)
+            backbone_model = ResNet9(in_channels, num_class)
         else:
-            return ResNet18(in_channels, num_class)
+            backbone_model = ResNet18(in_channels, num_class)
+
+        # Create a learner and set its backbone model
+        learner = MyLearner()
+        learner.backbone_model = backbone_model
+        
+        # Absorb `training_info` to keep track of training
+        if learner.hasattr('training_info'):
+            for key in training_info:
+                learner.training_info[key] = training_info[key]
+        else:
+            learner.training_info = training_info
+
+        return learner
--- AutoDL_sample_code_submission/Auto_Image/model_final.py
+++ AutoDL_sample_code_submission/Auto_Image/model_final.py
@@ -1,14 +1,31 @@
-from learner import MyLearner
+from learner import MyLearner           # Not used for now
+from hp_optimizer import MyHPOptimizer
+from data_ingestor import MyDataIngestor
 
 
 class Model():
 
     def __init__(self, metadata):
-        self.learner = MyLearner()
+        # `training_info` starts with the metadata and can be updated to store 
+        # any intermediate training information
+        self.training_info = {}
+        self.training_info['metadata'] = metadata
+
+        # Instantiate an HPOptimizer
+        self.hp_optimizer = MyHPOptimizer()
+
+        # Instantiate a DataIngestor
+        self.data_ingestor = MyDataIngestor()
+
+        # Get learner using the HPOptimizer. 
+        # The learner can absorb `training_info` as its own attribute
+        self.learner = self.hp_optimizer.fit(self.training_info)
 
     def train(self, dataset):
-        self.predictor = self.learner.learn(dataset)
+        dataset_uw = self.data_ingestor(dataset)    # uw for universal workflow
+        self.predictor = self.learner.learn(dataset_uw)
 
     def test(self, dataset):
-        predictions = self.predictor.predict(dataset)
+        dataset_uw = self.data_ingestor(dataset)
+        predictions = self.predictor.predict(dataset_uw)
         return predictions
